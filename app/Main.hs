77f4a349c1b78e61bd258dbc7c8dcf32a7878329
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-06-06 17:19:45 +0200

    Find words from letters

---
+++ app/Main.hs
@@ -0,0 +1,40 @@
+#!/usr/bin/env runhaskell
+
+module Main where
+
+import Data.List (delete, find, sortOn, (\\))
+import System.Environment
+
+main = do
+  args <- getArgs
+  file <- readFile (args !! 0)
+  let allWords = lines file
+  let allowed = args !! 1
+  let words = spellableWords allowed allWords
+  mapM_ putStrLn (sortOn length words)
+
+spellableWords :: [Char] -> [String] -> [String]
+spellableWords chars words =
+  filter (restrict chars) possibleWords
+  where
+    forbiddenChars = ['a'..'z'] \\ chars
+    possibleWords = removeForbidden forbiddenChars words
+
+removeForbidden :: [Char] -> [String] -> [String]
+removeForbidden forbiddenChars words =
+  filter containsForbidden words
+  where
+    containsForbidden word =
+      not $ containsOne forbiddenChars word
+
+containsOne :: [Char] -> String -> Bool
+containsOne []     word = False
+containsOne (c:cs) word =
+  case elem c word of
+    True  -> True
+    False -> containsOne cs word
+
+restrict :: [Char] -> [Char] -> Bool
+restrict _      []   = True
+restrict []     word = False
+restrict (l:ls) word = restrict ls (delete l word)
