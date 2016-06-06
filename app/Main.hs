3405ed5d19a7cc35ad2d65dbff6e9b02a2be0ea2
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-06-07 01:43:44 +0200

    Move code to Lib module

--- app/Main.hs
+++ app/Main.hs
@@ -2,39 +2,14 @@
 
 module Main where
 
-import Data.List (delete, find, sortOn, (\\))
+import Data.List (sortOn)
 import System.Environment
+import Lib
 
 main = do
   args <- getArgs
   file <- readFile (args !! 0)
   let allWords = lines file
   let allowed = args !! 1
-  let words = spellableWords allowed allWords
+  let words = spellableWords allowed ['a'..'z'] allWords
   mapM_ putStrLn (sortOn length words)
-
-spellableWords :: [Char] -> [String] -> [String]
-spellableWords chars dict =
-  filter (restrict chars) possibleWords
-  where
-    forbiddenChars = ['a'..'z'] \\ chars
-    possibleWords = removeForbidden forbiddenChars dict
-
-removeForbidden :: [Char] -> [String] -> [String]
-removeForbidden forbiddenChars dict =
-  filter containsForbidden dict
-  where
-    containsForbidden word =
-      not $ containsOne forbiddenChars word
-
-containsOne :: [Char] -> String -> Bool
-containsOne []     word = False
-containsOne (c:cs) word =
-  case elem c word of
-    True  -> True
-    False -> containsOne cs word
-
-restrict :: [Char] -> [Char] -> Bool
-restrict _      []   = True
-restrict []     word = False
-restrict (l:ls) word = restrict ls (delete l word)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
 module Lib
-    ( someFunc
+    ( spellableWords
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (delete, (\\))
+
+spellableWords :: [Char] -> [Char] -> [String] -> [String]
+spellableWords chars alphabet dict =
+  filter (restrict chars) possibleWords
+  where
+    forbiddenChars = alphabet \\ chars
+    possibleWords = removeForbidden forbiddenChars dict
+
+removeForbidden :: [Char] -> [String] -> [String]
+removeForbidden forbiddenChars dict =
+  filter containsForbidden dict
+  where
+    containsForbidden word =
+      not $ containsOne forbiddenChars word
+
+containsOne :: [Char] -> String -> Bool
+containsOne []     word = False
+containsOne (c:cs) word =
+  case elem c word of
+    True  -> True
+    False -> containsOne cs word
+
+restrict :: [Char] -> [Char] -> Bool
+restrict _      []   = True
+restrict []     word = False
+restrict (l:ls) word = restrict ls (delete l word)
