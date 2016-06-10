60926471850d8fcf9d558054f49ca15abf0ba4f1
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-06-10 21:12:07 +0200

    Interactive mode

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,40 @@
 
 module Main where
 
+import Control.Monad (forever)
 import Data.List (sortOn)
-import System.Environment
 import Lib
+import System.Environment
+import System.IO (hFlush, stdout)
+
+alphabet = ['a'..'z']
 
 main = do
   args <- getArgs
-  file <- readFile (args !! 0)
-  let allWords = lines file
-  let allowed = args !! 1
-  let words = spellableWords allowed ['a'..'z'] allWords
+  dict <- getDict args
+  case length args of
+    2 -> printOrdered words
+      where chars = args !! 1
+            words = spellableWords chars alphabet dict
+    _ -> interactive dict alphabet
+
+getDict args =
+  case length args of
+    0 -> readDict "/usr/share/dict/words"
+    _ -> readDict (args !! 0)
+
+readDict fileName = do
+  file <- readFile fileName
+  return $ lines file
+
+printOrdered words =
   mapM_ putStrLn (sortOn length words)
+
+interactive dict alphabet = forever $ do
+  putStr "> "
+  hFlush stdout
+  chars <- getLine
+  let words = spellableWords chars alphabet dict
+  printOrdered words
+  putStrLn $ concat $ replicate (length chars) "-"
+  putStrLn chars
