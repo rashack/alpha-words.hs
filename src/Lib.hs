19abbe2f714acfbbef2b59bdb799a6e65d55ab08
Kjell Andreassen <andreassen.kjell@gmail.com>
2016-06-11 20:55:40 +0200

    Fix hlint suggestions

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,36 +6,34 @@ module Lib
 
 import Data.List (delete, (\\))
 
-spellableWords :: [Char] -> [Char] -> [String] -> [String]
+spellableWords :: String -> String -> [String] -> [String]
 spellableWords chars alphabet dict =
   filter (restrict chars) possibleWords
   where
     forbiddenChars = alphabet \\ chars
     possibleWords = removeForbidden forbiddenChars dict
 
-removeForbidden :: [Char] -> [String] -> [String]
+removeForbidden :: String -> [String] -> [String]
 removeForbidden forbiddenChars dict =
   filter containsForbidden dict
   where
     containsForbidden word =
       not $ containsOne forbiddenChars word
 
-containsOne :: [Char] -> String -> Bool
+containsOne :: String -> String -> Bool
 containsOne []     word = False
 containsOne (c:cs) word =
-  case c `elem` word of
-    True  -> True
-    False -> containsOne cs word
+  (c `elem` word) || containsOne cs word
 
-restrict :: [Char] -> [Char] -> Bool
+restrict :: String -> String -> Bool
 restrict _      []   = True
 restrict []     word = False
 restrict (l:ls) word = restrict ls (delete l word)
 
-onlyWordsWith :: [Char] -> [String] -> [String]
+onlyWordsWith :: String -> [String] -> [String]
 onlyWordsWith chars words =
   filter (hasAllChars chars) words
 
-hasAllChars :: String -> [Char] -> Bool
+hasAllChars :: String -> String -> Bool
 hasAllChars word chars =
   chars \\ word == ""
